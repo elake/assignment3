585e4bce212251496bcce2f6eab163f56404efcb
cmput296 <cmput296@ubuntu.(none)>
2013-02-28 09:47:47 -0700

    Added tests, renamed cost function to match specification, moved edmonton graph building outside of if: __main to match specifications.

--- server2.py
+++ server2.py
@@ -1,24 +1,53 @@
 import digraph
 
 
-def edgecost(e):
+def cost_distance(e):
     '''
     edge cost function for an edge composed of two vertices; returns the 
     Euclidean distance between the two vertices in 100,000ths of degrees
     '''
     x1, y1 = vertices[e[0]]
     x2, y2 = vertices[e[1]]
+    if 0: # For testing int vs float distance returns
+        return int(eucDist(x1, y1, x2, y2))
     return eucDist(x1, y1, x2, y2)
 
 def least_cost_path(G, start, dest, cost):
     '''
     implementation of Dijkstra's algorithm, written in majority by Dr. Jim
     Hoover though the path traceback was written by Eldon Lake
+    The first two tests were taken from a discussion forum post by Dr.
+    Michael Bowling
+    Tests:
+    >>> G = digraph.Digraph([(0, 1), (1, 2), (3, 4), (0, 2), (2, 4)])
+    >>> least_cost_path(G, 0, 4, lambda e: 1)
+    [0, 2, 4]
+    >>> least_cost_path(G, 0, 4, lambda e: (e[0] - e[1]) ** 2)
+    [0, 1, 2, 4]
+    >>> G = digraph.Digraph([(0, 1), (1, 2), (2, 6), (3, 4), (0, 6)])
+    >>> least_cost_path(G, 0, 6, lambda e: 1)
+    [0, 6]
+    >>> least_cost_path(G, 0, 0, lambda e: 1)
+    [0]
+    >>> least_cost_path(G, 0, 4, lambda e: 1) is None
+    True
+    >>> least_cost_path(G, 5, 6, lambda e: (e[0] - e[1]) ** 2) is None
+    True
+    >>> least_cost_path(G, 9, 11, lambda e: (e[0] - e[1]) ** 2) is None
+    True
+    >>> G = digraph.Digraph([(0, 1), (0, 2), (1, 2), (1, 3), (2, 3)])
+    >>> least_cost_path(G, 0, 3, lambda e: e[0])
+    [0, 1, 3]
+    >>> G = digraph.Digraph([(1, 3), (1, 2), (2, 6), (3, 9), (6, 9)])
+    >>> least_cost_path(G, 1, 9, lambda e: e[1] - e[0] + 1)
+    [1, 3, 9]
     '''
     todo = {start: 0}
     parent = {}
     visited = set()
-
+    # Return none if start or dest cannot be found
+    if start not in G.vertices() or dest not in G.vertices():
+        return None
     while todo and dest not in visited:
         cur = min(todo, key=todo.get)
         c = todo[cur]
@@ -43,6 +72,15 @@ def least_cost_path(G, start, dest, cost):
 def eucDist(x1, y1, x2, y2):
     '''
     Euclidean distance between (x1, y1) and (x2, y2)
+    Tests:
+    >>> eucDist(0, 0, 4, 3)
+    5.0
+    >>> eucDist(0, 0, 68, 285)
+    293.0
+    >>> eucDist(0, 0, 0, 0)
+    0.0
+    >>> eucDist(45, 900, 65, 150)
+    750.2666192761077
     '''
     cost = ((x1-x2)**2 + (y1-y2)**2)**0.5
     return cost
@@ -58,7 +96,7 @@ def nearestVertex(x, y):
             dist = eucDist(x, y, v[1][0], v[1][1]) 
     return closest
 
-if __name__ == "__main__":
+if 1:
     import doctest
     doctest.testmod()
     
@@ -92,13 +130,22 @@ if __name__ == "__main__":
     # make a digraph of digraph edges, vertices will be filled in automatically
     edmonton = digraph.Digraph(digraphEdges) 
 
+# Test path example at the end of page 2 in assignment3.pdf
+correct_path = [314088878, 314088877, 629239018, 314089059,
+                314089060, 629239023, 629239027, 629239028]
+our_path = least_cost_path(edmonton, nearestVertex(5365488, -11333914),
+                nearestVertex(5364727, -11335890), cost_distance)
+if correct_path != our_path:
+    print("Example path failed!")
+
+if __name__ == "__main__":
     # Code for processing route finding requests here
     while 1: # continuously wait for input
         trip = input('Awaiting input:').split(" ")
         x1, y1, x2, y2 = trip
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         path = least_cost_path(edmonton, nearestVertex(x1, y1),
-                        nearestVertex(x2, y2), edgecost)
+                        nearestVertex(x2, y2), cost_distance)
         print(len(path))
         for v in path:
             print(vertices[v][0], vertices[v][1])
