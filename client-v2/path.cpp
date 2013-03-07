be0ef03d661a3a324641b3cb90808a8016c73b89
cmput296 <cmput296@ubuntu.(none)>
2013-03-06 19:35:33 -0700

    Added "edge clipping"

--- client-v2/path.cpp
+++ client-v2/path.cpp
@@ -5,6 +5,7 @@
 
 #include "map.h"
 #include "serial_handling.h"
+#include "path.h"
 
 #define DEBUG_PATH6
 
@@ -136,8 +137,118 @@ void draw_path(uint16_t length, coord_t path[], char map_num) {
     x1 = longitude_to_x(map_num, line_to.lon) - screen_map_x;
     y1 = latitude_to_y(map_num, line_to.lat) - screen_map_y;
 
-    draw_line(x0, y0, x1, y1);
+    if (box_contains(x0, y0) && box_contains(x1, y1)) {
+      draw_line(x0, y0, x1, y1);
+    }
+    else {
+      clip_draw(x0, y0, x1, y1);
+    }
   }
     // if current and prev points are visible then draw a line
     // tft.drawLine(prev_x, prev_y, cur_x, cur_y, BLUE);
+}
+
+int box_contains(int16_t x0, int16_t y0)
+{
+  // globals: display_window_height, display_window_width, msg_window_height
+  return (x0 >= 0 && x0 <= 120) && 
+    (y0 >= 0 && y0 <= (148));
+}
+
+const int xmin = 0;
+const int xmax = 128;
+const int ymin = 0;
+const int ymax = 148;
+
+typedef int OutCode;
+
+const int INSIDE = 0; // 0000
+const int LEFT = 1;   // 0001
+const int RIGHT = 2;  // 0010
+const int BOTTOM = 4; // 0100
+const int TOP = 8;    // 1000
+ 
+// Compute the bit code for a point (x, y) using the clip rectangle
+// bounded diagonally by (xmin, ymin), and (xmax, ymax)
+ 
+// ASSUME THAT xmax, xmin, ymax and ymin are global constants.
+ 
+OutCode ComputeOutCode(double x, double y)
+{
+        OutCode code;
+ 
+        code = INSIDE;          // initialised as being inside of clip window
+ 
+        if (x < xmin)           // to the left of clip window
+                code |= LEFT;
+        else if (x > xmax)      // to the right of clip window
+                code |= RIGHT;
+        if (y < ymin)           // below the clip window
+                code |= BOTTOM;
+        else if (y > ymax)      // above the clip window
+                code |= TOP;
+ 
+        return code;
+}
+ 
+// Cohen–Sutherland clipping algorithm clips a line from
+// P0 = (x0, y0) to P1 = (x1, y1) against a rectangle with 
+// diagonal from (xmin, ymin) to (xmax, ymax).
+void clip_draw(double x0, double y0, double x1, double y1)
+{
+      
+        OutCode outcode0 = ComputeOutCode(x0, y0);
+        OutCode outcode1 = ComputeOutCode(x1, y1);
+        bool accept = false;
+ 
+        while (true) {
+                if (!(outcode0 | outcode1)) { // Bitwise
+                        accept = true;
+                        break;
+                } else if (outcode0 & outcode1) { // Bit
+		  tft.fillRect(0, 75, 25, 125, GREEN);
+                        break;
+      
+                } else {
+                        // failed both tests, so calcula
+                        // from an outside point to an i
+                        double x, y;
+ 
+                        // At least one endpoint is outs
+                        OutCode outcodeOut = outcode0 ? outcode0 : outcode1;
+ 
+                        // Now find the intersection point;
+                        // use formulas y = y0 + slope *
+                        if (outcodeOut & TOP) {         
+                                x = x0 + (x1 - x0) * (ymax - y0) / (y1 - y0);
+                                y = ymax;
+                        } else if (outcodeOut & BOTTOM) {
+                                x = x0 + (x1 - x0) * (ymin - y0) / (y1 - y0);
+                                y = ymin;
+                        } else if (outcodeOut & RIGHT) { 
+                                y = y0 + (y1 - y0) * (xmax - x0) / (x1 - x0);
+                                x = xmax;
+                        } else if (outcodeOut & LEFT) {  
+                                y = y0 + (y1 - y0) * (xmin - x0) / (x1 - x0);
+                                x = xmin;
+                        }
+ 
+                        // Now we move outside point to i
+                        // and get ready for next pass.
+                        if (outcodeOut == outcode0) {
+                                x0 = x;
+                                y0 = y;
+                                outcode0 = ComputeOutCode(x0, y0);
+                        } else {
+                                x1 = x;
+                                y1 = y;
+                                outcode1 = ComputeOutCode(x1, y1);
+                        }
+                }
+        }
+        if (accept) {
+               // Following functions are left for implem
+               // their platform (OpenGL/graphics.h etc.)
+               draw_line(x0, y0, x1, y1);
+        }
 }
--- client-v2/path.h
+++ client-v2/path.h
@@ -6,5 +6,9 @@ extern int16_t path_errno;
 uint8_t read_path(uint16_t *length_p, coord_t *path_p[]);
 void draw_path(uint16_t length, coord_t path[], char map_num);
 uint8_t is_coord_visible(coord_t point);
+int box_contains(int16_t x0, int16_t y0);
+typedef int OutCode;
+OutCode ComputeOutCode(double x, double y);
+void clip_draw(double x0, double y0, double x1, double y1);
 
 #endif
